721b26790ddbe33adbcd6fc9c35fe1070edd81b0
Algieba <algieba.king@gmail.com>
2023-08-13 21:15:36 +0800

    initial: create the first version of this robot framework

--- WeChatCore/wechat_contact_info.py
+++ WeChatCore/wechat_contact_info.py
@@ -3,7 +3,7 @@ from typing import List
 from wcferry import Wcf
 
 
-class ContactsBase:
+class WeChatContactsBase:
     def __init__(self, wcf: Wcf):
         self.wcf = wcf
         self._counts = self.wcf.query_sql("MicroMsg.db", "SELECT COUNT(*) FROM Contact;")[0]["COUNT(*)"]
@@ -29,24 +29,30 @@ class ContactsBase:
         return contacts
 
 
-class ChatRoom:
+class WeChatRoom:
     def __init__(self, room_code: str, room_name: str):
         """
-        room_code: unique code for every chat room, e.g. 111111@chatroom
-        room_name: the real name of the chat room
+        :param room_code: unique code for every chat room, e.g. 111111@chatroom
+        :param room_name: the real name of the chat room
         """
         self.room_code = room_code
         self.room_name = room_name
 
 
-class ChatRooms(ContactsBase):
+class WeChatRooms(WeChatContactsBase):
     def __init__(self, wcf: Wcf):
+        """
+        Get all ChatRooms information
+        :param wcf:
+        """
         super().__init__(wcf=wcf)
         self.__chat_rooms = self.__load_chat_rooms()
+        self.__room_code2room_name = {room.room_code: room.room_name for room in self.__chat_rooms}
 
-    def __load_chat_rooms(self) -> List[ChatRoom]:
+    def __load_chat_rooms(self) -> List[WeChatRoom]:
         """
         Load all chat rooms into self
+        :return: All chat rooms
         """
         chat_rooms = list()
         contacts = self._all_contacts
@@ -55,88 +61,118 @@ class ChatRooms(ContactsBase):
                 continue
             room_code = contact["UserName"]
             room_name = contact["NickName"]
-            chat_rooms.append(ChatRoom(room_code=room_code, room_name=room_name))
+            chat_rooms.append(WeChatRoom(room_code=room_code, room_name=room_name))
         return chat_rooms
 
-    def get_chat_rooms(self) -> List[ChatRoom]:
+    def get_chat_rooms(self) -> List[WeChatRoom]:
         """
-        return all chat rooms
+        return all chat rooms, directly get self.__chat_rooms
+        :return:
         """
         return self.__chat_rooms
+
+    def from_room_id_to_room_name(self, room_id: str):
+        """
+        get room name from room id
+        :param room_id:
+        :return:
+        """
+        try:
+            name = self.__room_code2room_name[room_id]
+            return name
+        except Exception as e:
+            err = f"no rood name for room id :{room_id}"
+            raise Exception(err)
 
     def update_chat_rooms(self):
         """
         update/fresh the chat rooms list
+        :return:
         """
         self.__chat_rooms = self.__load_chat_rooms()
+        self.__room_code2room_name = {room.room_code: room.room_name for room in self.__chat_rooms}
 
 
-class Contact:
+class WeChatContact:
     def __init__(self, contact_code: str, alias: str, nick_name: str, type_number: int):
         """
-        contact_code: unique contact code
-        alias: alias of contact
-        nick_name: the nickname of a contact
-        type_number: used for check if it's a friends or comes from a room
+        :param contact_code: unique contact code
+        :param alias: alias of contact
+        :param nick_name: the nickname of a contact
+        :param type_number: used for check if it's a friends or comes from a room
         """
         self.contact_code = contact_code
         self.alias = alias
         self.nick_name = nick_name
-        self.is_friend = True if type_number == 0 else False
+        self.is_friend = True if type_number == 3 else False
 
 
-class Contacts(ContactsBase):
+class WeChatContacts(WeChatContactsBase):
     def __init__(self, wcf: Wcf):
+        """
+        Get all contacts information, groups friends included
+        :param wcf:
+        """
         super().__init__(wcf=wcf)
         self.__contacts = self.__load_contacts()
+        self.__wxid2wxname = {contact.contact_code: contact.nick_name for contact in self.__contacts}
 
-    def __load_contacts(self) -> List[Contact]:
+    def __load_contacts(self) -> List[WeChatContact]:
         """
         load contacts to self
+        :return: 
         """
         contacts_result = list()
 
         contacts = self._all_contacts
         for contact in contacts:
             type_number = contact["Type"]
-            if type_number != 0 and type_number != 4:
+            if type_number != 3 and type_number != 4:
                 # type 0 -> friends, type 1 internal account, type 2 group
                 continue
             contact_code = contact["UserName"]
             alias = contact["Alias"]
             nick_name = contact["NickName"]
-            contacts_result.append(Contact(contact_code=contact_code,
-                                           alias=alias,
-                                           nick_name=nick_name,
-                                           type_number=type_number))
+            contacts_result.append(WeChatContact(contact_code=contact_code,
+                                                 alias=alias,
+                                                 nick_name=nick_name,
+                                                 type_number=type_number))
         return contacts_result
 
-    def get_contacts(self) -> List[Contact]:
+    def get_contacts(self) -> List[WeChatContact]:
         """
         get contacts
+        :return:
         """
         return self.__contacts
+
+    def wxid2wxname(self, wxid: str):
+        return self.__wxid2wxname[wxid]
 
     def update_contacts(self):
         """
         update contacts information
+        :return:
         """
         self.__contacts = self.__load_contacts()
+        self.__wxid2wxname = {contact.contact_code: contact.nick_name for contact in self.__contacts}
 
 
 class ContactInfo:
     def __init__(self, wcf: Wcf):
         self.wcf = wcf
-        counts: int = self.wcf.query_sql("MicroMsg.db", "SELECT COUNT(*) FROM Contact;")[0]["COUNT(*)"]
-        offset = 4
+        table_name = "ChatRoom"
+        counts: int = self.wcf.query_sql("MicroMsg.db", f"SELECT COUNT(*) FROM {table_name};")[0]["COUNT(*)"]
+        offset = 1
         page = int(counts / offset)
         remains = counts % offset
         contacts = list()
         for i in range(page):
-            contact = self.wcf.query_sql("MicroMsg.db", f"SELECT * FROM Contact LIMIT {offset} OFFSET {i * offset};")
+            contact = self.wcf.query_sql("MicroMsg.db",
+                                         f"SELECT * FROM {table_name} LIMIT {offset} OFFSET {i * offset};")
             contacts.extend(contact)
         contacts.extend(
-            self.wcf.query_sql("MicroMsg.db", f"SELECT * FROM Contact LIMIT {remains} OFFSET {page * offset};"))
+            self.wcf.query_sql("MicroMsg.db", f"SELECT * FROM {table_name} LIMIT {remains} OFFSET {page * offset};"))
         ...
 
         contacts = self.wcf.query_sql("MicroMsg.db", "SELECT * FROM Contact;")
@@ -156,11 +192,9 @@ class ContactInfo:
                 continue
             infos[table_name] = (self.wcf.query_sql("MicroMsg.db", f"SELECT * FROM {table_name}"))
 
-        ...
-
-
 if __name__ == "__main__":
     wcf = Wcf(debug=True)
-    rooms = ChatRooms(wcf=wcf).get_chat_rooms()
-    contacts = Contacts(wcf=wcf).get_contacts()
+    ContactInfo(wcf=wcf)
+    rooms = WeChatRooms(wcf=wcf).get_chat_rooms()
+    contacts = WeChatContacts(wcf=wcf).get_contacts()
     ...
